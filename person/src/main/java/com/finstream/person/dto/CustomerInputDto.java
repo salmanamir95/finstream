a937a46d6cb41be30d190791eee2dc0b6950ed34
salmanamir95 <mmsa155j3@gmail.com>
2026-09-02 15:58:10 +0500

    dto import error

--- person/src/main/java/com/finstream/person/dto/CustomerInputDto.java
+++ person/src/main/java/com/finstream/person/dto/CustomerInputDto.java
@@ -1,5 +1,15 @@
 package com.finstream.person.dto;
 
+
+import com.finstream.person.regChannel.RegistrationChannel;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
 @Getter
 @Setter
 public abstract class CustomerInputDto {
